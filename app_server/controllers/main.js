454c32be7a700a03431cdd0c55289b1e442bf52a
Sai3605 <22eg106a42@anurag.edu.in>
2024-09-21 20:29:01 +0530

    first commit

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,31 +1,9 @@
-// controllers/main.js
-
-// Method to handle the home page
-exports.index = (req, res) => {
-    res.render('index', { title: 'Home' });
-};
-
-// Method to handle the signin page
-exports.signin = (req, res) => {
-    res.render('signin', { title: 'Sign In' });
-};
-
-// Method to handle the location info page
-exports.location = (req, res) => {
-    res.render('index', { title: 'Location info' });
-};
-
-// Method to handle the review page
-exports.review = (req, res) => {
-    res.render('index', { title: 'Review' });
-};
-
-// Method to handle the about page
-exports.about = (req, res) => {
-    res.render('index', { title: 'About' });
-};
-
-// Method to handle the register page
-exports.register = (req, res) => {
-    res.render('register', { title: 'Register' });
-};
+module.exports.index=function(req,res){
+    res.render('index', { title: 'Sai Charan' });
+   };
+   module.exports.signin=function(req,res){
+    res.render('signin', { title: 'Sai Charan' });
+   };
+   module.exports.review=function(req,res){
+    res.render('review', { title: 'Sai Charan' });
+   };
